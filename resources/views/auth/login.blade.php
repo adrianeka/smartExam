04e13b3ae2c941f250f4174e14117f58545124ab
Surya Akbar Hilmi Saputra <hilmisaputra.y3@gmail.com>
2026-05-13 12:51:55 +0700

    forget password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -27,7 +27,13 @@
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
-
+        <div class="flex items-center justify-end mt-4">
+            @if (Route::has('password.request'))
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                    {{ __('Lupa password?') }}
+                </a>
+            @endif
+        </div>
         <!-- Remember Me -->
         {{-- <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -22,7 +22,6 @@
         <!-- Confirm Password -->
         <div class="mt-4">
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
                                 type="password"
                                 name="password_confirmation" required autocomplete="new-password" />
