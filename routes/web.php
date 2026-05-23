4b11de822cdf51942be77e0577dd0fc258730da3
Naufal_abdullah <lnaufa90@gmail.com>
2026-05-23 20:41:04 +0700

    redirecet web to login

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserActivationController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('login');
 });
 
 Route::get('/dashboard', function () {
